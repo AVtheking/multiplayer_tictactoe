15d14feb5ca5cd1a570a0d44525a44abc3f383e2
Ankit Varshney <lyfflame45@gmail.com>
2023-09-08 20:08:32 +0530

    uploading server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const PORT =  3000;
+const PORT =  process.env.PORT|| 3000;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
